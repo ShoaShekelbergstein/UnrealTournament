5f2e8e8a02bd8afa7ddba538bd3ad10e8ca650df
Matt Oelfke <Matt.Oelfke@epicgames.com>
2017-01-10 18:10:36 -0500

    Fixed perf issues with the rocket salvo
    [CL 3253037 by Matt Oelfke in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Public/UTRocketSalvoBoost.h
+++ UnrealTournament/Source/UnrealTournament/Public/UTRocketSalvoBoost.h
@@ -50,25 +50,25 @@ public:
 		TArray<APawn*> Targets;
 		for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; ++It)
 		{
-			if (It->IsValid() && 
-				It->Get() != UTOwner && 
-				!It->Get()->bTearOff && 
+			if (It->IsValid() &&
+				It->Get() != UTOwner &&
+				!It->Get()->bTearOff &&
 				(It->Get()->GetActorLocation() - MyLoc).Size() < TargetingRange &&
 				(GS == nullptr || !GS->OnSameTeam(It->Get(), UTOwner)))
 			{
 				Targets.Add(It->Get());
 			}
-			Targets.Sort([this, MyLoc](const APawn& A, const APawn& B) { return (A.GetActorLocation() - MyLoc).Size() < (B.GetActorLocation() - MyLoc).Size(); });
-			FActorSpawnParameters Params;
-			Params.Instigator = UTOwner;
-			const FVector SpawnLoc = UTOwner->GetActorLocation() + UTOwner->GetActorRotation().Vector() * UTOwner->GetSimpleCollisionRadius() + FVector(0.0f, 0.0f, UTOwner->GetSimpleCollisionHalfHeight() * 0.9f);
-			for (int32 i = FMath::Min<int32>(Targets.Num(), MaxTargets) - 1; i >= 0; i--)
+		}
+		Targets.Sort([this, MyLoc](const APawn& A, const APawn& B) { return (A.GetActorLocation() - MyLoc).Size() < (B.GetActorLocation() - MyLoc).Size(); });
+		FActorSpawnParameters Params;
+		Params.Instigator = UTOwner;
+		const FVector SpawnLoc = UTOwner->GetActorLocation() + UTOwner->GetActorRotation().Vector() * UTOwner->GetSimpleCollisionRadius() + FVector(0.0f, 0.0f, UTOwner->GetSimpleCollisionHalfHeight() * 0.9f);
+		for (int32 i = FMath::Min<int32>(Targets.Num(), MaxTargets) - 1; i >= 0; i--)
+		{
+			AUTProj_Rocket* Rocket = GetWorld()->SpawnActor<AUTProj_Rocket>(ProjClass, SpawnLoc, FRotator(90.0f, 0.0f, 0.0f), Params);
+			if (Rocket != nullptr)
 			{
-				AUTProj_Rocket* Rocket = GetWorld()->SpawnActor<AUTProj_Rocket>(ProjClass, SpawnLoc, FRotator(90.0f, 0.0f, 0.0f), Params);
-				if (Rocket != nullptr)
-				{
-					Rocket->TargetActor = Targets[i];
-				}
+				Rocket->TargetActor = Targets[i];
 			}
 		}
 	}
