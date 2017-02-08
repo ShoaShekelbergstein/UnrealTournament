f83cfa91e35ce2e105a955a576c3a53649a3c4b4
Steve Polge <steve.polge@epicgames.com>
2017-02-07 20:08:05 -0500

    Lightning Rifle C++, converted charge bar to C++
    [CL 3291403 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTWeap_LightningRifle.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTWeap_LightningRifle.cpp
@@ -8,13 +8,25 @@
 AUTWeap_LightningRifle::AUTWeap_LightningRifle(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	DefaultGroup = 9;
-	Group = 9;
-/*	WeaponCustomizationTag = EpicWeaponCustomizationTags::GrenadeLauncher;
+}
 
-	DetonationAfterFireDelay = 0.3f;
+void AUTWeap_LightningRifle::DrawWeaponCrosshair_Implementation(UUTHUDWidget* WeaponHudWidget, float RenderDelta)
+{
+	Super::DrawWeaponCrosshair_Implementation(WeaponHudWidget, RenderDelta);
 
-	ShotsStatsName = NAME_BioLauncherShots;
-	HitsStatsName = NAME_BioLauncherHits;
-	HighlightText = NSLOCTEXT("Weapon", "GrenadeHighlightText", "Hot Potato");*/
+	if ((ChargePct > 0.f) && WeaponHudWidget && WeaponHudWidget->UTHUDOwner)
+	{
+		float Width = 150.f;
+		float Height = 21.f;
+		float WidthScale = WeaponHudWidget->GetRenderScale();
+		float HeightScale = WidthScale;
+		WidthScale *= 0.75f;
+		//	WeaponHudWidget->DrawTexture(WeaponHudWidget->UTHUDOwner->HUDAtlas, 0.f, 96.f, Scale*Width, Scale*Height, 127, 671, Width, Height, 0.7f, FLinearColor::White, FVector2D(0.5f, 0.5f));
+		WeaponHudWidget->DrawTexture(WeaponHudWidget->UTHUDOwner->HUDAtlas, 0.f, 32.f, WidthScale*Width*ChargePct, HeightScale*Height, 127, 641, Width, Height, 0.7f, BLUEHUDCOLOR, FVector2D(0.5f, 0.5f));
+		if (ChargePct == 1.f)
+		{
+			WeaponHudWidget->DrawText(NSLOCTEXT("LightningRifle", "Charged", "CHARGED"), 0.f, 28.f, WeaponHudWidget->UTHUDOwner->TinyFont, 1.f, 1.f, FLinearColor::Yellow, ETextHorzPos::Center, ETextVertPos::Center);
+		}
+		WeaponHudWidget->DrawTexture(WeaponHudWidget->UTHUDOwner->HUDAtlas, 0.f, 32.f, WidthScale*Width, HeightScale*Height, 127, 612, Width, Height, 1.f, FLinearColor::White, FVector2D(0.5f, 0.5f));
+	}
 }
