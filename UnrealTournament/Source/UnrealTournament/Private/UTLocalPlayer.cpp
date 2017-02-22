1c43bf0f29fb0bc01998532b546e03e788946bf2
Joe Wilcox <Joe.Wilcox@epicgames.com>
2017-02-22 11:07:23 -0500

    Added an analytic event for when matchmaking has to restart
    [CL 3316759 by Joe Wilcox in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTLocalPlayer.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTLocalPlayer.cpp
@@ -4894,11 +4894,35 @@ void UUTLocalPlayer::ChallengeCompleted(FName ChallengeTag, int32 Stars)
 	}
 }
 
+//Special markup for Analytics event so they show up properly in grafana. Should be eventually moved to UTAnalytics.
+/*
+* @EventName QuickmatchFull
+*
+* @Trigger Sent when a player was added to a quickmatch server but it was full
+*
+* @Type Sent by the Client
+*
+* @EventParam ChallengeTag string Challenge Tag name
+* @EventParam Stars int32 Stars this challenge is worth
+* @EventParam TotalStars int32 Total earned stars
+*
+* @Comments
+*/
+
+
 bool UUTLocalPlayer::QuickMatchCheckFull()
 {
 #if !UE_SERVER
 	if (QuickMatchDialog.IsValid() || MatchmakingDialog.IsValid())
 	{
+		if (FUTAnalytics::IsAvailable())
+		{
+			TArray<FAnalyticsEventAttribute> ParamArray;
+			ParamArray.Add(FAnalyticsEventAttribute(TEXT("LastMatchmakingSessionId"), LastMatchmakingSessionId));
+			FUTAnalytics::SetClientInitialParameters(Cast<AUTPlayerController>(PlayerController), ParamArray, false);
+			FUTAnalytics::GetProvider().RecordEvent(TEXT("QuickmatchFull"), ParamArray);
+		}
+
 		FTimerHandle TmpHandle;
 		GetWorld()->GetTimerManager().SetTimer(TmpHandle, this, &UUTLocalPlayer::RestartQuickMatch, 0.5f, false);
 		return true;
