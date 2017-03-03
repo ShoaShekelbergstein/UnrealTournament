fa2ae1238a160f01aa5cbe01ca46af5164694876
Steve Polge <steve.polge@epicgames.com>
2017-03-03 09:37:14 -0500

    Adjusted rocket seeking.
    [CL 3331358 by Steve Polge in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/UTProj_Rocket.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/UTProj_Rocket.cpp
@@ -25,7 +25,7 @@ AUTProj_Rocket::AUTProj_Rocket(const class FObjectInitializer& ObjectInitializer
 	bLeadTarget = true;
 	bRocketTeamSet = false;
 	MaxLeadDistance = 2000.f;
-	MinSeekDistance = 200.f;
+	MinSeekDistance = 100.f;
 	MaxTargetLockIndicatorDistance = 5000.f;
 }
 
