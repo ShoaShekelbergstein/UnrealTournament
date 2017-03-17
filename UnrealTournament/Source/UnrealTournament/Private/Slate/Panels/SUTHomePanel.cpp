7aba85545d8ffe708bba7f7891f9ed301c621f30
Peter Knepley <peter.knepley@epicgames.com>
2017-03-16 21:40:29 -0400

    Remix lan search block
    [CL 3351103 by Peter Knepley in UT-Clean branch]

--- UnrealTournament/Source/UnrealTournament/Private/Slate/Panels/SUTHomePanel.cpp
+++ UnrealTournament/Source/UnrealTournament/Private/Slate/Panels/SUTHomePanel.cpp
@@ -87,7 +87,12 @@ void SUTHomePanel::Tick( const FGeometry& AllottedGeometry, const double InCurre
 void SUTHomePanel::CheckForLanServers()
 {
 	TSharedPtr<SUTServerBrowserPanel> Browser = PlayerOwner->GetServerBrowser(false);
-	if (Browser.IsValid() && (Browser->GetBrowserState() == EBrowserState::RefreshInProgress || !PlayerOwner->LastRankedMatchSessionId.IsEmpty()))
+	if (Browser.IsValid() && Browser->GetBrowserState() == EBrowserState::RefreshInProgress)
+	{
+		return;
+	}
+
+	if (!PlayerOwner->LastRankedMatchSessionId.IsEmpty())
 	{
 		return;
 	}
