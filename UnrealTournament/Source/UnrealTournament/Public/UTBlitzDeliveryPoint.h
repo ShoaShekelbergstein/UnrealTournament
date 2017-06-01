00df9054b8f85efb4a12271917f4f7d76ee88612
Steve Polge <steve.polge@epicgames.com>
2017-06-01 09:35:09 -0400

    Added UTBlitzDeliveryPoint actor, replaced Loh delivery point with it.
    [CL 3469265 by Steve Polge in UT-Clean branch]

---
+++ UnrealTournament/Source/UnrealTournament/Public/UTBlitzDeliveryPoint.h
@@ -0,0 +1,14 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "UTCTFFlagBase.h"
+#include "UTBlitzDeliveryPoint.generated.h"
+
+UCLASS(HideCategories = GameObject)
+class UNREALTOURNAMENT_API AUTBlitzDeliveryPoint : public AUTCTFFlagBase
+{
+	GENERATED_UCLASS_BODY()
+
+};
+
